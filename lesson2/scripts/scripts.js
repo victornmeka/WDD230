bd4f7a7e6b06c17dff9db26046a76b3b09a116f4
victornmeka <nme22001@byui.edu>
2023-01-14 23:30:24 +0100

    week 1 done week 2 begin

--- lesson2/scripts/scripts.js
+++ lesson2/scripts/scripts.js
@@ -4,7 +4,5 @@ document.getElementById('year').textContent = new Date().toLocaleDateString('en-
 const option2 = {month: 'numeric', day: 'numeric', year: 'numeric'}
 document.getElementById('date').textContent = new Date().toLocaleDateString('en-us', option2)
 
-const option1 = new Date(document.lastModified)
+const option1 = new Date()
 document.getElementById('time').textContent = new Date().toLocaleTimeString('en-us', option1) 
-
-
